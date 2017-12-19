a9cdf3842dfeee5d3330fe124974887ec5a87a14
Tony Riverkey <tonyarroyave@gmail.com>
2017-12-18 21:33:59 -0600

    Initial commit

---
+++ Price_Hour_BTC.py
@@ -0,0 +1,23 @@
+import bitso
+import csv
+import time
+
+with open('BTC_Price.csv', 'w') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['TIME','ASK','BID','HIGH','LAST','LOW','VWAP','VOLUME'])
+
+api = bitso.Api()
+transcurrido = 0
+while(True):
+    time.sleep(5)
+    transcurrido = transcurrido + 5
+    print('Sigo vivo...')
+    if (transcurrido >= 10):
+        transcurrido = 0
+        tick = api.ticker('btc_mxn')
+        with open('BTC_Price.csv', 'a') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow([tick.created_at,tick.ask,tick.bid,tick.high,tick.last,tick.low,tick.vwap,tick.volume])
+            print('Tengo valores :D!')
